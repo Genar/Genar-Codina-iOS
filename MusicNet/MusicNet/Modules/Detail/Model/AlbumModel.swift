0ec68fbefdcbbeb0d9e09725535b047f03f40e31
Genar Codina Reverter <genar.codina.reverter@everis.com>
2021-02-27 23:35:12 +0100

    ADD: Album Model

---
+++ MusicNet/MusicNet/Modules/Detail/Model/AlbumModel.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumModel.swift
+//  MusicNet
+//
+//  Created by Genaro Codina Reverter on 27/2/21.
+//
+
+import Foundation
+
+public struct AlbumModel: Comparable {
+    
+    let id: String
+    let name: String
+    let image: Data?
+    let imageUrl: URL?
+    let releaseDate: String?
+    
+    public static func < (lhs: AlbumModel, rhs: AlbumModel) -> Bool {
+        
+        return lhs.name < rhs.name
+    }
+    
+}
+
